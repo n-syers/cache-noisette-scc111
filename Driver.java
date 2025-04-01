16752a3e81e72fa8caa91163fde5028a785df215
n-syers <n.syers@lancaster.ac.uk>
2025-04-01 10:52:38 +0100

    Added MenuController.java to control menu functionality
    The purpose of the menu controller is to control the functionallity of button presses for the menu bar and the main menu.

--- Driver.java
+++ Driver.java
@@ -3,13 +3,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.*;
 
+/**
+ * This class holds the main() for all other files but also initialises a JFrame for all components to be added to.
+ */
 public class Driver {
-
+    private static JFrame cacheNoisetteJFrame = new JFrame("Cache Noisette Game"); // Initialise a JFrame with Title accessible via all classes.
+    private static MenuController menuController = new MenuController(); // Used to control the menus in the program.
     public static void main(String[] args) {
         Dimension windowSize = new Dimension(900, 900); // Size of Window
         
         //initialise the JFrame to hold all game content
-        JFrame cacheNoisetteJFrame = new JFrame("Cache Noisette Game"); // Initialise a JFrame with Title
         cacheNoisetteJFrame.setVisible(true);
         cacheNoisetteJFrame.setSize(windowSize);
         cacheNoisetteJFrame.setMinimumSize(windowSize);
@@ -21,17 +24,19 @@ public class Driver {
         cacheNoisetteJFrame.setLayout(new BorderLayout());
 
         // In-JFrame Title creation and properties
-        JLabel titleLable = new JLabel(); // Title label for the JFrame
+        JLabel titleLable = new JLabel();
         titleLable.setText("Cache Noisette Game");
         titleLable.setFont(new java.awt.Font("Segoe UI Black", 1, 18));
         titleLable.setHorizontalAlignment(SwingConstants.CENTER);
         titleLable.setMaximumSize(new java.awt.Dimension(200, 75));
         titleLable.setMinimumSize(new java.awt.Dimension(200, 75));
         titleLable.setPreferredSize(new java.awt.Dimension(200, 75));
+        cacheNoisetteJFrame.add(titleLable, BorderLayout.NORTH);
 
-        // Add components to JFrame
-        cacheNoisetteJFrame.getContentPane().add(titleLable, BorderLayout.NORTH);
-
+        // Add the menus to the JFrame
+        cacheNoisetteJFrame.add(menuController.getButtonMenu(), BorderLayout.CENTER);
+        cacheNoisetteJFrame.setJMenuBar(menuController.getMenuBar());
     }
+    
 
 }
---
+++ MenuController.java
@@ -0,0 +1,121 @@
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.*;
+import java.io.IOException;
+import java.nio.file.*;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+
+public class MenuController implements ActionListener{
+    private JPanel buttonMenu = new JPanel(); // Holds all JSwing Objects for Main Menu
+    private JPanel buttonGroup = new JPanel(); // Holds both main menu buttons.
+    private JButton playButton = new JButton(); // Initiates the loading of Level 1
+    private JButton quitButton = new JButton(); // Exits Program with Status 0
+    private JMenuBar gameMenuBar = new JMenuBar(); // Menu bar for during gameplay
+    private JMenu levelSelectorMenu = new JMenu(); // Holds all levels that can be selected
+    private JMenuItem customLevelMenuItem = new JMenuItem(); // Button for Custom Level Loading
+
+    /**
+     * Constructor for MenuController.
+     * Populates the buttonMenu and gameMenuBar with buttons and actionListeners.
+     */
+    MenuController(){
+        // Set initialPlayMenu layout to GridBagLayout
+        buttonMenu.setLayout(new GridBagLayout());
+
+        // Set buttonGroup Properties
+        buttonGroup.setMaximumSize(new Dimension(400, 300));
+        buttonGroup.setMinimumSize(new Dimension(400, 150));
+        buttonGroup.setPreferredSize(new Dimension(400, 150));
+        buttonGroup.setLayout(new GridLayout(2, 1, 0, 20));
+
+        // Set playButton Properties
+        playButton.setText("Click to Play");
+        playButton.setMaximumSize(null);
+        playButton.setMinimumSize(null);
+        playButton.setPreferredSize(new Dimension(200, 75));
+        playButton.addActionListener(this);
+        buttonGroup.add(playButton);
+
+        // Set quitButton Properties
+        quitButton.setText("Quit Game");
+        quitButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        quitButton.setMaximumSize(null);
+        quitButton.setMinimumSize(null);
+        quitButton.setPreferredSize(new Dimension(200, 75));
+        quitButton.addActionListener(this);
+        buttonGroup.add(quitButton);
+
+        // Add buttonGroup to initialPlayMenu
+        buttonMenu.add(buttonGroup, new GridBagConstraints());
+
+        /* Create the Menu Bar */
+
+        // Set levelSelectorMenu Properties
+        levelSelectorMenu.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        levelSelectorMenu.setText("Levels");
+
+        // Set customLevelMenuItem Properties and Add to levelSelectorMenu
+        customLevelMenuItem.setText("Load Custom Level");
+        customLevelMenuItem.addActionListener(this);
+        levelSelectorMenu.add(customLevelMenuItem);
+
+        /*
+         * For each file in directoryPath that matches pattern "level*.bmp"
+         * Get fileName and create String "Level ##" for menuItem
+         * Add menuItem to menu with actionListener.
+         * Try and Catch for errors.
+         */
+        Path directoryPath = Paths.get("assets/levels/");
+        String menuItemNameString = "";
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directoryPath, "level*.bmp")) {
+            for (Path entry : stream) {
+                System.out.println("Found Level File: " + entry.getFileName());
+                menuItemNameString = entry.getFileName().toString();
+                menuItemNameString = menuItemNameString.replace("level", "Level ").replace(".bmp", "");
+                JMenuItem levelMenuItem = new JMenuItem();
+                levelMenuItem.setText(menuItemNameString);
+                levelMenuItem.addActionListener(this); //Not optimal as event listener will need to check button text.
+                levelSelectorMenu.add(levelMenuItem);
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+        gameMenuBar.add(levelSelectorMenu);
+    }
+    
+    /**
+     * Returns the JPanel for the button menu.
+     * @return gameButtonMenu
+     */
+    public JPanel getButtonMenu(){
+        return this.buttonMenu;
+    }
+
+    /**
+     * Returns the JMenuBar for the main menu bar.
+     * @return gameMenuBar
+     */
+    public JMenuBar getMenuBar(){
+        return this.gameMenuBar;
+    }
+    /**
+     * Holds all functionality behind button presses.
+     */
+    public void actionPerformed(ActionEvent e){
+
+        // load the first level of the game.
+        if(e.getSource() == playButton){
+             System.out.println("Play Button Pressed");
+             buttonMenu.setVisible(false);
+        };
+
+        // exit the game with status 0
+        if(e.getSource() == quitButton){
+             System.out.println("Quit Button Pressed, Goodbye!");
+             System.exit(0);
+        };
+    }
+}
