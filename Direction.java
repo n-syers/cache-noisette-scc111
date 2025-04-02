e17d957bf8e346fb4c0f7aa7d4d2e04bfba69878
n-syers <n.syers@lancaster.ac.uk>
2025-04-03 00:14:29 +0100

    Added a rotationDegree value to each direction. Accessable via getValue()

--- Direction.java
+++ Direction.java
@@ -1,6 +1,16 @@
 public enum Direction {
-    NORTH,
-    SOUTH,
-    EAST,
-    WEST
+    NORTH(0),
+    SOUTH(180),
+    EAST(90),
+    WEST(270);
+
+    public final int rotationDegree;
+
+    private Direction(int rotationDegree){
+        this.rotationDegree = rotationDegree;
+    }
+
+    public int getValue(Direction direction){
+        return direction.rotationDegree;
+    }
 }
