c5f6ea45812c209db084ce379f146979fbcdfbd0
n-syers <n.syers@lancaster.ac.uk>
2025-04-03 12:23:06 +0100

    Removed Hole.java constructor to simplify code

--- Hole.java
+++ Hole.java
@@ -6,24 +6,11 @@ import java.awt.Point;
  */
 public class Hole extends GamePiece{
     private int size = 1; // size of the GamePiece on the board
-    private Picture[] imageArray; // Array of Pictures
-    private Point[] piecePositions; // Array of gamePiece positions.
+    private Picture[] imageArray = new Picture[]{new Picture("assets\\icons\\Hole.png", 0), new Picture("assets\\icons\\HoleNut.png", 0)}; // Array of Pictures
+    private Point[] piecePositions = new Point[]{new Point(0,0)}; // Array of gamePiece positions.
     private Point headPoint; // Position of the head on the board
-
     private Boolean hasNut = false; // If the hole has a nut, False by default
-    /**
-     * Constructor for Hole class. initiates piecePositions, headPoint, and imageArray with their respective values.
-     * @param x The X-Cord for headPoint
-     * @param y The Y-Cord for headPoint
-     */
-    public Hole(int x, int y){
-        piecePositions = new Point[size];
-        piecePositions[0] = new Point(0,0);
-        headPoint = new Point(x, y);
-        imageArray = new Picture[size+1];
-        imageArray[0] = new Picture("assets/icons/Hole.png", 0);
-        imageArray[1] = new Picture("assets/icons/HoleNut.png", 0);
-    }
+    
     /**
      * Determine whether the hole has a nut or not
      * @return A Boolean value of hasNut
