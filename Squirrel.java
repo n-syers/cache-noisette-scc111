5f5398bf03052ff96ace14119758f900344cc313
n-syers <n.syers@lancaster.ac.uk>
2025-04-03 10:21:53 +0100

    Corrected the file paths for the pictures in Squirrel.java

--- Squirrel.java
+++ Squirrel.java
@@ -33,33 +33,33 @@ public class Squirrel extends GamePiece{
     public void setPictures(Colour colour, int rotation){
         switch (colour) {
             case BLACK:
-                imageArray[0] = new Picture("assets/icons/BlackSquirrel1", rotation);
-                imageArray[1] = new Picture("assets/icons/BlackSquirrel1Nut", rotation);
-                imageArray[2] = new Picture("assets/icons/BlackSquirrel2", rotation);
-                imageArray[3] = new Picture("assets/icons/SquirrelFlower", rotation);
+                imageArray[0] = new Picture("assets\\icons\\BlackSquirrel1.png", rotation);
+                imageArray[1] = new Picture("assets\\icons\\BlackSquirrel1Nut.png", rotation);
+                imageArray[2] = new Picture("assets\\icons\\BlackSquirrel2.png", rotation);
+                imageArray[3] = new Picture("assets\\icons\\SquirrelFlower.png", rotation);
                 break;
 
             case BROWN:
-                imageArray[0] = new Picture("assets/icons/BrownSquirrel1", rotation);
-                imageArray[1] = new Picture("assets/icons/BrownSquirrel1Nut", rotation);
-                imageArray[2] = new Picture("assets/icons/BrownSquirrel2", rotation);
-                imageArray[3] = new Picture("assets/icons/SquirrelFlower", rotation);
+                imageArray[0] = new Picture("assets\\icons\\BrownSquirrel1.png", rotation);
+                imageArray[1] = new Picture("assets\\icons\\BrownSquirrel1Nut.png", rotation);
+                imageArray[2] = new Picture("assets\\icons\\BrownSquirrel2.png", rotation);
+                imageArray[3] = new Picture("assets\\icons\\SquirrelFlower.png", rotation);
                 break;
 
             case GREY:
-                imageArray[0] = new Picture("assets/icons/GreySquirrel1", rotation);
-                imageArray[1] = new Picture("assets/icons/GreySquirrel1Nut", rotation);
-                imageArray[2] = new Picture("assets/icons/GreySquirrel2", rotation);
+                imageArray[0] = new Picture("assets\\icons\\GreySquirrel1.png", rotation);
+                imageArray[1] = new Picture("assets\\icons\\GreySquirrel1Nut.png", rotation);
+                imageArray[2] = new Picture("assets\\icons\\GreySquirrel2.png", rotation);
                 break;
 
             case RED:
-                imageArray[0] = new Picture("assets/icons/RedSquirrel1", rotation);
-                imageArray[1] = new Picture("assets/icons/RedSquirrel1Nut", rotation);
-                imageArray[2] = new Picture("assets/icons/RedSquirrel2", rotation);
+                imageArray[0] = new Picture("assets\\icons\\RedSquirrel1.png", rotation);
+                imageArray[1] = new Picture("assets\\icons\\RedSquirrel1Nut.png", rotation);
+                imageArray[2] = new Picture("assets\\icons\\RedSquirrel2.png", rotation);
                 break;
 
             default:
-                System.err.println("An error occurred: Unable to determin squirrel colour");
+                System.err.println("An error occurred: Unable to determine squirrel colour");
                 break;
         }
     }
