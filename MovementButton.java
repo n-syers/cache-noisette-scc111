af6daa26c801f197e47c664245420a7534111235
n-syers <n.syers@lancaster.ac.uk>
2025-04-03 22:41:52 +0100

    Added MovementButton class to handle movement buttons. Extends JButton

---
+++ MovementButton.java
@@ -0,0 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+public class MovementButton extends JButton implements ActionListener{
+    Picture arrowPicture;
+    GameBoard gameBoard;
+    Direction direction;
+
+    public enum Size{
+        LARGE,
+        SMALL
+    };
+
+    public MovementButton(GameBoard gameBoard, Direction direction, Size arrowSize){
+        this.gameBoard = gameBoard;
+        this.direction = direction;
+        switch (arrowSize) {
+            case LARGE:
+                arrowPicture = new Picture("assets\\icons\\BigArrow.png", direction.getValue(direction));
+                break;
+            case SMALL:
+                arrowPicture = new Picture("assets\\icons\\Arrow.png", direction.getValue(direction));
+                break;
+            default:
+                System.out.println("An Error Occured: Could Not Find Arrow Size, Set To Small");
+                arrowPicture = new Picture("assets\\icons\\Arrow.png", direction.getValue(direction));
+                break;
+        }
+        this.setIcon(arrowPicture);
+        this.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       gameBoard.movePiece(gameBoard.getSelectedGamePiece(), direction);
+    }
+}
