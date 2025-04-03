a130b3d8f8943ebe32e075f0e0b6c5c7690c98a7
n-syers <n.syers@lancaster.ac.uk>
2025-04-03 15:04:59 +0100

    Added Squirrel Object creation for loadLevel in LevelController, Fixed logic bug with detecting direction

--- LevelController.java
+++ LevelController.java
@@ -36,36 +36,40 @@ public class LevelController {
                         System.out.println("Squirrel Head Space Detected");
                         int[][] rgbArray = new int[4][2];
                         rgbArray[0][0] = bufferedImage.getRGB(1+col, row);
-                        rgbArray[0][1] = 0;
+                        rgbArray[0][1] = 180;
                         rgbArray[1][0] = bufferedImage.getRGB(col, 1+row);
                         rgbArray[1][1] = 90;
                         rgbArray[2][0] = bufferedImage.getRGB(2+col, 1+row);
                         rgbArray[2][1] = 270;
                         rgbArray[3][0] = bufferedImage.getRGB(1+col, 2+row);
-                        rgbArray[3][1] = 180;
+                        rgbArray[3][1] = 0;
                         for (int i = 0; i < rgbArray.length; i++) {
                             red = (rgbArray[i][0] >> 16) & 0xFF;
                             green = (rgbArray[i][0] >> 8) & 0xFF;
                             blue = rgbArray[i][0] & 0xFF;
                             if (red == 255 && green == 127 && blue == 39) {
                                 System.out.println("Red Squirrel Head Space Detected with rotation " + rgbArray[i][1]);
+                                gameBoard.placePiece(new Squirrel(Colour.RED, Direction.getDirection(rgbArray[i][1]), row/4, col/4, 2), row/4, col/4, Direction.getDirection(rgbArray[i][1]), 2);
                             } else if (red == 195 && green == 195 && blue == 195) {
                                 System.out.println("Grey Squirrel Head Space Detected with rotation " + rgbArray[i][1]);
+                                gameBoard.placePiece(new Squirrel(Colour.GREY, Direction.getDirection(rgbArray[i][1]), row/4, col/4, 2), row/4, col/4, Direction.getDirection(rgbArray[i][1]), 2);
                             } else if (red == 239 && green == 228 && blue == 176) {
                                System.out.println("Brown Squirrel Head Space Detected with rotation " + rgbArray[i][1]);
+                               gameBoard.placePiece(new Squirrel(Colour.BROWN, Direction.getDirection(rgbArray[i][1]), row/4, col/4, 3), row/4, col/4, Direction.getDirection(rgbArray[i][1]), 3);
                             } else if (red == 0 && green == 0 && blue == 0) {
                                 System.out.println("Black Squirrel Head Detected with rotation " + rgbArray[i][1]);
+                                gameBoard.placePiece(new Squirrel(Colour.BLACK, Direction.getDirection(rgbArray[i][1]), row/4, col/4, 3), row/4, col/4, Direction.getDirection(rgbArray[i][1]), 3);
                             }
                         }
                     } else if (red == 237 && green == 28 && blue == 36) {
                         System.out.println("Flower Space Detected");
-                    } else if (red == 34 && green == 177 && blue == 76) {
-                        System.out.println("Duel Flower Space Detected");
+                        gameBoard.placePiece(new Flower(), row/4, col/4, Direction.NORTH, 1);
                     }
                 }
             }
         } catch (Exception e) {
             System.err.println("An error occurred: " + e.getMessage());
         }
+        gameBoard.refreshFrame();
     }
 }
