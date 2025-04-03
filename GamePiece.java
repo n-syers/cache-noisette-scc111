f89e21ca4d0ab21c474bcc2d474084fc7690ae90
n-syers <n.syers@lancaster.ac.uk>
2025-04-03 12:36:56 +0100

    Reworked GamePiece into an abstract class with abstract methods

--- GamePiece.java
+++ GamePiece.java
@@ -1,50 +1,35 @@
 import java.awt.Point;
 
-public class GamePiece {
+public abstract class GamePiece {
     protected GameBoard gameBoard; // Reference to GameBoard for all pieces.
-    private int size; // size of the GamePiece on the board
-    private Picture[] imageArray; // Array of Pictures
-    private Point[] piecePositions; // Array of gamePiece positions.
-    private Point headPoint; // Position of the head on the board
     
     /**
      * Retrieves an array of imageFilenames for a game piece.
      * @return imageFilenames picture array
      */
-    public Picture[] getPictures(){
-        return this.imageArray;
-    }
+    public abstract Picture[] getPictures();
 
     /**
      * Retrieves an array of points for a game piece.
      * @return piecePositions point array
      */
-    public Point[] getPiecePositions(){
-        return this.piecePositions;
-    }
+    public abstract Point[] getPiecePositions();
     /**
      * Places GamePiece at specific cordinates given a direction
      * @param x The X-Cord on gameBoard
      * @param y The Y-Cord on gameBoard
      * @param direction Direction enum for rotation of gamePiece
      */
-    public void setPosition(int x, int y, Direction direction){
-        headPoint = new Point(x, y);
-        gameBoard.placePiece(this, x, y, direction, size);
-    }
+    public abstract void setPosition(int x, int y, Direction direction);
     /**
      * This function returns the point value of headPoint.
      * @return Point headPoint
      */
-    public Point getHeadPosition(){
-        return headPoint;
-    }
+    public abstract Point getHeadPosition();
 
     /**
      * Determins whether a piece can be walked over or not. By default is true but might be overriden.
      * @return True by default
      */
-    public Boolean isWalkable(){
-        return true; // Default response, can be overriden
-    }
+    public abstract Boolean isWalkable();
 }
