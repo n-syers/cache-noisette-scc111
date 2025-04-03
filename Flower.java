eb08b78b9d0df7177602934a306b801286893ef4
n-syers <n.syers@lancaster.ac.uk>
2025-04-03 12:43:35 +0100

    Added Flower class to represent a flower game piece (Extends GamePiece)

---
+++ Flower.java
@@ -0,0 +1,35 @@
+import java.awt.Point;
+
+public class Flower extends GamePiece{
+    private int size = 1; // size of the GamePiece on the board
+    private Picture[] imageArray = new Picture[]{new Picture("assets\\icons\\Flower.png", 0)}; // Array of Pictures
+    private Point[] piecePositions = new Point[]{new Point(0,0)}; // Array of gamePiece positions.
+    private Point headPoint; // Position of the head on the board
+
+    // Inherited Functions
+    @Override
+    public Picture[] getPictures(){
+        return imageArray;
+    }
+
+    @Override
+    public Point getHeadPosition(){
+        return headPoint;
+    }
+
+    @Override
+    public void setPosition(int x, int y, Direction direction){
+        headPoint = new Point(x, y);
+        gameBoard.placePiece(this, x, y, direction, size);
+    }
+
+    @Override
+    public Point[] getPiecePositions(){
+        return piecePositions;
+    }
+
+    @Override  
+    public Boolean isWalkable() {
+        return true;
+    }
+}
