2f2ef8619f82249f8a4004b929e40c07129137cc
n-syers <n.syers@lancaster.ac.uk>
2025-04-04 19:52:49 +0100

    Added javadoc comments to MovementButton class

--- MovementButton.java
+++ MovementButton.java
@@ -2,17 +2,33 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
-
+/**
+ * This class represents a {@code Movement Button} instance, which extends the {@code JButton} class and implements the {@code ActionListener} interface.
+ * This class defines the behaviour, attributes, and actions a MovementButton instance can perform within the game. 
+ * 
+ * @see JButton
+ * @see ActionListener
+ */
 public class MovementButton extends JButton implements ActionListener{
-    Picture arrowPicture;
-    GameBoard gameBoard;
-    Direction direction;
+    Picture arrowPicture; // Holds an instance of Picture for JButton
+    GameBoard gameBoard; // Holds a reference to GameBoard
+    Direction direction; // Holds the direction enum of the arrow
 
+    /**
+     * This enum is used to indicate the size of the MovementButton
+     * and what arrow to show.
+     */
     public enum Size{
         LARGE,
         SMALL
     };
-
+    /**
+     * Constructs an instance of {@code MovementButton}, while initialising {@code gameBoard}, {@code direction}, and {@code arrowPicture}
+     * This creates a {@code JButton} with formatting and assigns it an action listener.
+     * @param gameBoard The GameBoard instance to be referenced
+     * @param direction The Direction enum of the arrow
+     * @param arrowSize The Size enum of the arrow
+     */
     public MovementButton(GameBoard gameBoard, Direction direction, Size arrowSize){
         this.gameBoard = gameBoard;
         this.direction = direction;
