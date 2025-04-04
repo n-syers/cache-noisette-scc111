6b1b4f077cdf21ea045f278f1358d5961e6e6892
n-syers <n.syers@lancaster.ac.uk>
2025-04-04 13:36:20 +0100

    Created FileManager class. Moved readBitmapAsBufferedImage() from LevelController to FileManager

---
+++ FileManager.java
@@ -0,0 +1,36 @@
+import java.awt.image.BufferedImage;
+import java.io.RandomAccessFile;
+
+public class FileManager {
+    /**
+     * The function takes a directory path as a string and returns a BufferedImage using RandomAccessFile data streams. Byte data is read row-by-row and converted to RGB values and set on an empty bufferedImage.
+     * @param levelFilePath String of the file path.
+     * @return The bufferedImage after the image is created.
+     */
+    public static BufferedImage readBitmapAsBufferedImage(String levelFilePath){
+        try {
+            RandomAccessFile randomAccessFile = new RandomAccessFile(levelFilePath, "r");
+            randomAccessFile.skipBytes(54);
+            BufferedImage bufferedImage = new BufferedImage(15, 15, BufferedImage.TYPE_INT_RGB);
+            int rowLength = 15 * 3;
+            int paddedRowLength = (rowLength + 3) & ~3;
+            byte[] pixelData = new byte[paddedRowLength]; // 15x15 pixel image with 3 bytes per pixel rounded to the next multiple of 4
+            for (int y = 15 - 1; y >= 0; y--) {
+                randomAccessFile.readFully(pixelData); // Read one row of pixel data
+                for (int x = 0; x < 15; x++) {
+                    int blue = pixelData[x * 3] & 0xFF;
+                    int green = pixelData[x * 3 + 1] & 0xFF;
+                    int red = pixelData[x * 3 + 2] & 0xFF;
+                    int rgb = (red << 16) | (green << 8) | blue;
+                    bufferedImage.setRGB(x, y, rgb);
+                }
+            }
+            randomAccessFile.close();
+            return bufferedImage;
+        } catch (Exception e) {
+            System.err.println("(FILEMANAGER::readBitmapAsBufferedImage) An error occurred: " + e.getMessage());
+            return null;
+        }
+    }
+    
+}
--- LevelController.java
+++ LevelController.java
@@ -27,12 +27,11 @@ public class LevelController {
     public void loadLevel(int levelNumber){
         gameBoard.clearGameBoard();
         gameBoard.initialiseEmptyBoards();
-        gameBoard.refreshFrame();
         try {
             System.out.println("Loading Level " + levelNumber);
             File file = new File("assets/levels/level"+levelNumber+".bmp");
             activeLevelPath = file.getPath();
-            BufferedImage bufferedImage = getBufferedImageFromPath(activeLevelPath);
+            BufferedImage bufferedImage = FileManager.readBitmapAsBufferedImage(activeLevelPath);
             loadData(bufferedImage);
         } catch (Exception e) {
             System.err.println("(LEVELCONTROLLER::loadLevel) An error occurred: " + e.getMessage());
@@ -47,10 +46,9 @@ public class LevelController {
     public void loadLevelFromFilePath(String levelFilePath){
         gameBoard.clearGameBoard();
         gameBoard.initialiseEmptyBoards();
-        gameBoard.refreshFrame();
         try {
             System.out.println("Loading Custom Level...");
-            BufferedImage bufferedImage = getBufferedImageFromPath(levelFilePath);
+            BufferedImage bufferedImage = FileManager.readBitmapAsBufferedImage(levelFilePath);
             loadData(bufferedImage);
         } catch (Exception e) {
             System.err.println("(LEVELCONTROLLER::loadLevelFromFilePath) An error occurred: " + e.getMessage());
@@ -72,36 +70,6 @@ public class LevelController {
                 loadLevelFromFilePath(selectedFile.getAbsolutePath());
         } else {
                 System.out.println("No file selected");
-        }
-    }
-    /**
-     * The function takes a directory path as a string and returns a BufferedImage using RandomAccessFile data streams. Byte data is read row-by-row and converted to RGB values and set on an empty bufferedImage.
-     * @param levelFilePath String of the file path.
-     * @return The bufferedImage after the image is created.
-     */
-    private BufferedImage getBufferedImageFromPath(String levelFilePath){
-        try {
-            RandomAccessFile randomAccessFile = new RandomAccessFile(levelFilePath, "r");
-            randomAccessFile.skipBytes(54);
-            BufferedImage bufferedImage = new BufferedImage(15, 15, BufferedImage.TYPE_INT_RGB);
-            int rowLength = 15 * 3;
-            int paddedRowLength = (rowLength + 3) & ~3;
-            byte[] pixelData = new byte[paddedRowLength]; // 15x15 pixel image with 3 bytes per pixel rounded to the next multiple of 4
-            for (int y = 15 - 1; y >= 0; y--) {
-                randomAccessFile.readFully(pixelData); // Read one row of pixel data
-                for (int x = 0; x < 15; x++) {
-                    int blue = pixelData[x * 3] & 0xFF;
-                    int green = pixelData[x * 3 + 1] & 0xFF;
-                    int red = pixelData[x * 3 + 2] & 0xFF;
-                    int rgb = (red << 16) | (green << 8) | blue;
-                    bufferedImage.setRGB(x, y, rgb);
-                }
-            }
-            randomAccessFile.close();
-            return bufferedImage;
-        } catch (Exception e) {
-            System.err.println("(LEVELCONTROLLER::getBufferedImageFromPath) An error occurred: " + e.getMessage());
-            return null;
         }
     }
     /**
@@ -174,7 +142,7 @@ public class LevelController {
         }
     }
     /**
-     * This function calls loadLevel with the current levelNumber. Restarting the level.
+     * This function calls loadLevel with the current activeLevelPath. Restarting the level.
      */
     public void restartLevel(){
         loadLevelFromFilePath(activeLevelPath);
